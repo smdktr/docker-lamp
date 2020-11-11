4100acf7e7302817b4c39cccbcd05a4e9b98def9
smdktr <smdktr86@gmail.com>
2020-11-11 12:17:32 +0900

    init

---
+++ htdocs/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php:7.4-apache</title>
+</head>
+
+<body>
+  <p>LAMP環境構築に成功しました</p>
+  <?php phpinfo(); ?>
+</body>
+
+</html>
